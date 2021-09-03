eeb1d37bb2960ade942b99da9a115526f85f6f98
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2021-09-03 17:36:12 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,112 @@
+//Sondos Ahmad Aabed
+//1190652
+//Dr. Yousef Lab1
+
+//import java.util.Date;//because I didn't use it actually but put it because it's required 
+
+public class Car {
+	int currentYear=2020;
+    int currentMonth=11;
+  //I had this warring of the date methods are deprecated so I couldn't use, 
+  //but instead I initialized the year and month  
+	private String plateNo;
+	private String color;
+	private String manufactureby;
+
+	private int yearmanufacture;
+	private int monthmanufacture;
+	private int guaranteedueyear;
+	private int guaranteeduemonth;
+	
+	private double price; 
+	
+	public Car() {
+		
+	}
+	
+	public Car(String plateNo,int yearmanufacture ,int monthmanufacture ,String color,double
+			price,String manufactureby,int guaranteedueyear ,int guaranteeduemonth ) {
+		
+	}
+	
+	public int getYearManufacture() {
+		return yearmanufacture;
+	}
+	
+	public int getMonthManufacture() {
+		return monthmanufacture;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public String getCarAge() {
+		int ageYear;
+		int ageMonth;
+		int age;
+		ageYear =currentYear-yearmanufacture;
+		if(ageYear>0) {
+			ageYear=ageYear*12;//convert it to months
+		} 
+		ageMonth=currentMonth-monthmanufacture;
+		age=ageMonth+ageYear;	
+		String y= age +"months";  
+		return y ;// y is the string of car age in months
+	}	
+	
+	public String getmanufactureby() {
+		return manufactureby;
+	}	 
+
+	public String getCalculateGuarantee() {
+	    int remainingYears ;
+	    int remainingMonth ; 
+	    int remaining;
+		remainingYears=guaranteedueyear-currentYear;
+		if(remainingYears>=0) {
+			remainingYears=remainingYears*12;//convert years to months
+		}
+		else if(remainingYears<0) {
+			remainingYears=0;    
+		}
+		
+	    remainingMonth= guaranteeduemonth-currentMonth;
+	    if(remainingMonth<0) {
+			remainingMonth=0;    
+		}
+ 	    remaining=remainingYears+remainingMonth;	
+	  
+	 	
+		String s= remaining+"months";
+		return s;
+	}
+
+	public void setYearManufacture(int year) {
+		 yearmanufacture=year; 	
+	}
+
+	public void setMonthManufacture(int month) {
+		monthmanufacture=month;
+	}
+	
+	public void setColor(String newColor) {
+		color= newColor;
+	}
+	
+	public void setGuarantee(int month,int year) {
+		guaranteedueyear=year;
+		guaranteeduemonth=month;
+	}	
+	
+	public String printCarInfo() {
+		String s=""+ plateNo + yearmanufacture + monthmanufacture +color+ price+ manufactureby+guaranteedueyear+guaranteeduemonth; 
+		System.out.printf(s);
+		return s; 
+	}
+	
+} 
---
+++ DriverClass.java
@@ -0,0 +1,52 @@
+//import java.util.Date;//because I didn't use it actually but put it because it's required 
+public class DriverClass { 
+	public static void main(String []args) {
+    int currentYear=2020;
+    int currentMonth=11;
+    int x;//the guarantee due to month
+    
+    x=currentMonth+6;
+    if (x>12) {
+    	x=x-12;//the month of the next year
+    }
+    Car []cars= new Car[5];
+    //Initialize the array of cars
+    cars[0]=new Car("0123-A",currentYear,11,"red",50000.00,"Mercedes",currentYear,x );
+    cars[1]=new Car("1230-A",2016,8,"blue",40000.00,"BMW",currentYear,x );
+    cars[2]=new Car("2130-A",2011,5,"yellow",20000.00,"Mazda",currentYear,x );
+    cars[3]=new Car("1320-A",2013,9,"white",30000.00,"Kia",currentYear,x ); 
+    cars[4]=new Car("3210-A",2009,6,"green",10000.00,"Nissan",currentYear,x );
+    
+    //pass the array to the method printCarsInfo 
+    printCarsInfo(cars);
+    
+    //pass the array to the method
+    maxPrice(cars);
+	}
+	
+	public static void printCarsInfo( Car [ ] cars) {
+  //this method is to print all information about cars with ages and guarantee due to.	
+		for(int i=0;i<=4;i++) {
+			System.out.println(cars[i].getYearManufacture());
+			System.out.println(cars[i].getMonthManufacture());
+			//System.out.println(cars[i].getColor().toUpperCase());
+			System.out.println(cars[i].getPrice());
+			System.out.println(cars[i].getCarAge().toUpperCase());
+			System.out.println(cars[i].getmanufactureby().toUpperCase());
+			System.out.println(cars[i].getCalculateGuarantee());
+		} 
+	}
+	
+	public static Car maxPrice( Car [ ] cars) {
+ //this method is to return the maximum price of cars 
+		double maxPrice = cars[0].getPrice();
+	    int indexOfMaxPrice = -1;
+	    for(int i = 0; i < cars.length; i++) {
+	        if(cars[i].getPrice() > maxPrice) {
+	        	maxPrice = cars[i].getPrice();
+	        	indexOfMaxPrice = i;
+	        }
+	    }
+	    return cars[indexOfMaxPrice]; 
+	}
+} 
